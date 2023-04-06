0745ca5ece06cda85e46ba1e566cecec1973d26a
Disha <bidisha2125@gmail.com>
2023-04-06 13:12:35 +0530

    Add env variable

--- send_emails.py
+++ send_emails.py
@@ -1,4 +1,5 @@
 import  smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "python404app@gmail.com"
-    password = "ohrxtbqbtmsxsuzw"
+    password = os.getenv("PASSWORD")
 
     receiver = "python404app@gmail.com"
     context = ssl.create_default_context()
